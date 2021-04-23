b074ceaa60292caba92c1824f98cdf35a6468656
S.Ignatyuk <signatyuk89@gmail.com>
2021-04-23 17:06:12 +0300

    Initial commit

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,26 @@
+package com.game.entity;
+
+import java.util.Date;
+
+public class Player {
+
+    Long id;
+
+    String name;
+
+    String title;
+
+    Race race;
+
+    Profession profession;
+
+    Integer experience;
+
+    Integer level;
+
+    Integer untilNextLevel;
+
+    Date birthday;
+
+    Boolean banned;
+}
